7b466a56769b2bec6ae18e780ce185ce9b2cc821
shiva <ghassemzadeh.sh@gmail.com>
2025-03-08 21:13:27 +0100

    fix bug to start setting up routes with  express router

--- api/server.js
+++ api/server.js
@@ -128,14 +128,18 @@ server.delete("/api/messages/:id", (req, res) => {
   Lessons.removeMessage(id)
     .then((count) => {
       if (count > 0) {
-        res.status(200).json({ message: `message with id ${id} successful remove ` });
+        res.status(200).json({ message: `message with id ${id} successful ` });
       } else {
         res.status(404).json({ message: "no message with that id" });
       }
     })
     .catch((error) => {
-      res.status(500).json({ message: "error deleting message" });
+      res.status(500).json({ message: "" });
     });
+});
 
-    
-    module.exports = server
+server.listen(port, () => {
+  console.log(`${port}`);
+});
+
+module.exports = server;
